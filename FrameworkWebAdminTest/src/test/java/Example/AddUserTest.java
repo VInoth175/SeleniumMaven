f76c3b4269d1afa5c44cad85e6b83ec9f50dd068
Vinoth <Vinoth@10.0.0.103>
2019-05-28 19:52:47 -0500

    Testting

--- FrameworkWebAdminTest/src/test/java/Example/AddUserTest.java
+++ FrameworkWebAdminTest/src/test/java/Example/AddUserTest.java
@@ -36,7 +36,7 @@ public class AddUserTest extends Testbase {
 	
 	@Given("click the Adduser button")
 	public void click_the_Adduser_button() throws Exception {
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 		adduserpage = adminpage.clickadduser();
 		
 

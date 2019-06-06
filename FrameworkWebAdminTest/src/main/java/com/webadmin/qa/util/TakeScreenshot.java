1cb20b0f75c9d3e20db16985d7fe642f3b46d7bb
Vinoth <Vinoth@10.0.0.103>
2019-06-06 17:45:46 -0500

    update

--- FrameworkWebAdminTest/src/main/java/com/webadmin/qa/util/TakeScreenshot.java
+++ FrameworkWebAdminTest/src/main/java/com/webadmin/qa/util/TakeScreenshot.java
@@ -1,22 +1,22 @@
-//package com.webadmin.qa.util;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//
-//import org.openqa.selenium.OutputType;
-//import org.openqa.selenium.TakesScreenshot;
-//
-//import com.webadmin.qa.base.Testbase;
-//
-//public class TakeScreenshot extends Testbase{
-//
-//	public void screehshot() throws IOException
-//	{
-//		
-//	File SrcFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-//	FileUtils.copyFile(SrcFile, new File("C:\\Users\\Vinoth\\git\\repository1\\FrameworkWebAdminTest\\target\\CaptureScreenshot"));
-//		
-//	}
-//	
-//}
+package com.webadmin.qa.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.webadmin.qa.base.Testbase;
+
+public class TakeScreenshot extends Testbase{
+
+	public void screehshot() throws IOException
+	{
+		
+	File SrcFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(SrcFile, new File("C:\\Users\\Vinoth\\git\\repository1\\FrameworkWebAdminTest\\target\\CaptureScreenshot"));
+		
+	}
+	
+}
